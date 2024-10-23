078b0c652da2592b85b3ab72cc559c71353b42dc
JessRamz25 <jessramiisd.9th@gmail.com>
2024-10-23 15:33:08 -0400

    Just ignore bro

--- src/App.jsx
+++ src/App.jsx
@@ -16,18 +16,18 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>I just changed the header</h1>
+      <h1>K's Compost Bucket Map</h1>
       <h2>Just testing this haha</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Buckets Collected
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Pounds Collected (lbs.)
       </p>
     </>
   )
