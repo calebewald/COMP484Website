f49d538be238fdc810952ae997393ad68ae155da
calebewald <138894638+calebewald@users.noreply.github.com>
2024-10-30 15:13:58 -0400

    removed default elements, renamed title

--- src/map.jsx
+++ src/map.jsx
@@ -6,15 +6,12 @@ const Map = () => {
     const position = [42.29043180177482, -85.59973287209094]
 
     return (
-        <MapContainer center={position} zoom={17} scrollWheelZoom={false} style={{ height: '100vh', width: '100%' }}>
+        <MapContainer center={position} zoom={17} scrollWheelZoom={false} style={{ height: '100vh', width: '100vh' }}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             <Marker position={position}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
             </Marker>
         </MapContainer>
     )
