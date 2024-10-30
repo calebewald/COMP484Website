8445d1380db3ccaaf5ffa5217523f12f9b121895
calebewald <138894638+calebewald@users.noreply.github.com>
2024-10-30 15:26:05 -0400

    added a form on the bottom for number of pounds and buckets

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Map from './map.jsx'
+import CompostCounter from './CompostCounter.jsx'
 import './App.css'
 
 function App() {
@@ -10,6 +9,7 @@ function App() {
   return (
     <>
       <Map />
+      <CompostCounter />
     </>
   )
 }
