a394f3e72a2885ded6290f48aa2b5db5e7d51e60
LuisLizardo <LuisLizardo73@gmail.com>
2024-10-30 15:13:44 -0400

    Clickable Map
    Base version of being able to add markers too the map

--- src/App.jsx
+++ src/App.jsx
@@ -29,9 +29,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      {/* Render the map */}
       <Map />
     </>
   )
 }
 
-export default App
+export default App;
--- src/map.jsx
+++ src/map.jsx
@@ -1,23 +1,42 @@
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet'
+import { useState } from 'react'
 import 'leaflet/dist/leaflet.css'
 
+const ClickableMap = () => {
+    const [popupPositions, setPopupPositions] = useState([]); // Store multiple positions
 
-const Map = () => {
-    const position = [42.29043180177482, -85.59973287209094]
+    // This component listens for clicks on the map
+    const LocationMarkers = () => {
+        useMapEvents({
+            click(e) {
+                setPopupPositions((prevPositions) => [
+                    ...prevPositions, // Add new position to the array
+                    e.latlng
+                ]);
+            },
+        });
+
+        // Render markers for all stored popup positions
+        return popupPositions.map((position, index) => (
+            <Marker key={index} position={position}>
+                <Popup>
+                    You clicked here: <br /> {position.lat}, {position.lng}
+                </Popup>
+            </Marker>
+        ));
+    };
+
+    const centerPosition = [42.29043180177482, -85.59973287209094];
 
     return (
-        <MapContainer center={position} zoom={17} scrollWheelZoom={false} style={{ height: '100vh', width: '100%' }}>
+        <MapContainer center={centerPosition} zoom={17} scrollWheelZoom={false} style={{ height: '100vh', width: '100%' }}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={position}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+            <LocationMarkers /> {/* Listen for clicks and render markers for each click */}
         </MapContainer>
-    )
-}
+    );
+};
 
-export default Map 
+export default ClickableMap;
